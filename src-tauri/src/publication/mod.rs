05650580d72f080781af439d8a1158cdc8ce8102
Wellington Júnior <67232334+orangethewell@users.noreply.github.com>
2024-11-16 16:30:08 -0300

    refactor: :art: Add alias to `Publication` inside publication module

--- src-tauri/src/publication/mod.rs
+++ src-tauri/src/publication/mod.rs
@@ -2,9 +2,10 @@ pub mod manager_old;
 
 pub mod extension;
 
-pub mod tables;
-pub mod publication;
 pub mod manifest;
+pub mod publication;
+pub use publication::Publication;
+pub mod tables;
 pub use manifest::Manifest;
 pub mod catalog;
 

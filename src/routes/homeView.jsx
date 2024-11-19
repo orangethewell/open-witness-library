c938260c255407c0bc928ed03c0c41d591d97be9
Wellington Júnior <67232334+orangethewell@users.noreply.github.com>
2024-11-19 17:19:05 -0300

    feat: :sparkles: Implement home view

--- src/routes/homeView.jsx
+++ src/routes/homeView.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import Catalog from '../components/catalog';
 import { invoke } from '@tauri-apps/api/core';
-import { Button, Box } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
 import { Fab } from '@mui/material';
-import { Add } from '@mui/icons-material';
+import { Add, OpenInNew } from '@mui/icons-material';
 import { open } from '@tauri-apps/plugin-dialog';
+import PageLayoutSlider from '../components/transitions';
+import { useTranslation, Trans } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const [publications, setPublications] = useState([]);
+    const { t } = useTranslation();
+    const navigate = useNavigate();
     
-    const handleClick = async () => {
-        let publication_data = await invoke("catalog_get_list_from_type", {publicationType: "Book"})
-        setPublications(publication_data);
+    const handleClick = (_ev) => {
+        navigate("/library");
     };
 
     const addPublication = async () => {
@@ -28,13 +30,43 @@ const Home = () => {
     }
 
     return (
-        <Box>
-            <Button onClick={handleClick}>Update List</Button>
-            <Catalog publications={publications} />
+        <>
+            <PageLayoutSlider>
+                <Box
+                    sx={{
+                        textAlign: "center",
+                        paddingTop: 4,
+                        paddingBottom: 4,
+                        paddingLeft: 16,
+                        paddingRight: 16,
+                    }}
+                >
+                    <Typography variant="h4">
+                        <Trans i18nKey="home.welcome"/>
+                    </Typography>
+                    <Typography sx={{paddingBottom: 8}} variant="body1">
+                        <Trans i18nKey="home.description"/>
+                    </Typography>
+                    <Typography variant="body2" color='textSecondary'>
+                        <Trans i18nKey="home.disclaimer" components={{plus_icon: <Add/>}}/>
+                    </Typography>
+                    <div style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        gap: 16,
+                        paddingTop: 16,
+                        paddingBottom: 16,
+                    }}>
+                        <Button variant="contained" color='primary' href="https://www.jw.org/" target="_blank">{t("home.download_button")}<OpenInNew sx={{fontSize: "medium", marginLeft: 1}}/></Button>
+                        <Button variant="contained" color='primary' onClick={handleClick}>{t("home.local_button")}</Button>
+                    </div>
+                </Box>
+            </PageLayoutSlider>
             <Fab onClick={addPublication} style={{position: "fixed", bottom: 20, right: 20}} color="primary">
                 <Add/>
             </Fab>
-        </Box>
+        </>
     );
 };
 

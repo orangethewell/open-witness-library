66b6a435c5f5b147e319061535d3d06057a33b70
Wellington Júnior <67232334+orangethewell@users.noreply.github.com>
2024-11-24 21:48:15 -0300

    feat: :lipstick: Improve intial transition

--- src/components/transitions.jsx
+++ src/components/transitions.jsx
@@ -1,11 +1,17 @@
-import { Slide } from "@mui/material";
+import { Fade, Slide } from "@mui/material";
 
 const PageLayoutSlider = ({ children }) => {
     return (
-      <Slide easing={{enter: "cubic-bezier(0.250, 0.100, 0.250, 1.000)", exit: "cubic-bezier(0.250, 0.100, 0.250, 1.000)"}} direction="up" in={true} timeout={500}>
-        {children}
-      </Slide>
+      <Fade easing={{enter: "cubic-bezier(0.250, 0.100, 0.250, 1.000)", exit: "cubic-bezier(0.250, 0.100, 0.250, 1.000)"}} in={true} timeout={1000}>
+        <div>
+          <Slide easing={{enter: "cubic-bezier(0.250, 0.100, 0.250, 1.000)", exit: "cubic-bezier(0.250, 0.100, 0.250, 1.000)"}} direction="up" in={true} timeout={500}>
+              {children}
+          </Slide>
+        </div>
+      </Fade>
+
     )
   }
+  
 
 export default PageLayoutSlider;
